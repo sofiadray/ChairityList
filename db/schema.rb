5bd72bcc0f35079bf4be8c9c58e03a790e3133c8
Sofia <sofiadray@gmail.com>
2015-12-28 13:52:26 -0500

    migration order fixed

---
+++ db/schema.rb
@@ -0,0 +1,67 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20151228185123) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "charities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image"
+    t.string   "website"
+    t.boolean  "real?"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "charities", ["category_id"], name: "index_charities_on_category_id", using: :btree
+
+  create_table "charity_cities", force: :cascade do |t|
+    t.integer  "city_id"
+    t.integer  "charity_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "charity_cities", ["charity_id"], name: "index_charity_cities_on_charity_id", using: :btree
+  add_index "charity_cities", ["city_id"], name: "index_charity_cities_on_city_id", using: :btree
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "state"
+    t.string   "country"
+    t.string   "continent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "charities", "categories"
+  add_foreign_key "charity_cities", "charities"
+  add_foreign_key "charity_cities", "cities"
+end
