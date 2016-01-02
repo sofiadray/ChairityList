87051f1aa72fe0cce56e797d10f9f1c4da24e50b
Sofia <sofiadray@gmail.com>
2016-01-02 16:56:14 -0500

    ip routing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,11 @@ class HomeController < ApplicationController
 
   def index
     @category = Category.all
+
+    ##redirects to user's city page based on ip
+    # request = request.remote.ip
+    # city = @request.location.city
+    # redirect_to city_path(@city.id)
   end
 
 end
