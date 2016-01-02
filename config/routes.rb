c88254456f708c31d0dec1ce102b2df298b4aefa
Josh Salez <josh.salez@gmail.com>
2016-01-02 12:19:07 -0500

    resolve routes conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'home#index'
+  
+
+
+
+
+
   get '/about' => 'static#about'
   get '/faq' => 'static#faq'
   get '/contact' => 'static#contact'
