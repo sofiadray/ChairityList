1ff7ae0fa3bf69360ac6dc3acdb575bb1ebf7f07
Josh Salez <josh.salez@gmail.com>
2016-01-02 11:52:29 -0500

    added static content pages, routes and views

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,12 @@
+class StaticController < ApplicationController
+
+  def about
+  end
+
+  def faq
+  end
+
+  def contact
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'home#index'
+  get '/about' => 'static#about'
+  get '/faq' => 'static#faq'
+  get '/contact' => 'static#contact'
 
 end
