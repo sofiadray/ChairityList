8b3ac27c9598ff308890d15c8a36b0bd81c9b74e
ceceny7 <christinekim715@gmail.com>
2016-01-05 16:23:25 -0500

    charity mission statements

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160102231209) do
+ActiveRecord::Schema.define(version: 20160105211246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160102231209) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.text     "overview"
   end
 
   add_index "charities", ["category_id"], name: "index_charities_on_category_id", using: :btree
